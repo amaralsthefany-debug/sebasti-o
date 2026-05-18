b541d6c09e48a0069e8875fbf4133904259aa2a6
amaralsthefany-debug <amaral.sthefany@escola.pr.gov.br>
2026-05-18 09:28:52 -0300

    atualização

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
 
-for(let i=0;1 <botoes.length)
+for(let i=0;1 <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;1 <botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    
+}
